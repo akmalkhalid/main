74a353b6b7b906bea87640670410b2c7ca620ffc
Akmal <43359748+akmal104041@users.noreply.github.com>
2020-07-24 21:34:51 +0900

    Update loadLatest.js

--- loadLatest.js
+++ loadLatest.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 		success: function(xmlData){
 
 			var html_data = '';
+			var count = 0;
 			$("paper", xmlData).each(function(){
 
 					var year_data = $(this).find("year").text();
@@ -15,7 +16,7 @@ $(document).ready(function(){
 					var publisher_data = $(this).find("publisher").text();
 					var link_data = $(this).find("link").text();
 
-          if (year_data.indexOf('2020') > -1) {
+          if (year_data.indexOf('2020') > -1 && count != 5) {
 
             html_data += '<a href="' + link_data + '" target="_blank"><div class="each">';
   					html_data += '<div class=" gatsby-image-wrapper" style="position: relative; overflow: hidden; display: inline-block; width: 150px; height: 150px;">';
@@ -23,7 +24,8 @@ $(document).ready(function(){
   					html_data += 'loading="lazy" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; object-fit: cover; object-position: center center; opacity: 1; transition: none 0s ease 0s;" width="150" height="150">';
   					html_data += '</picture></div>';
   					html_data += '<div class="each-list-item"><h2>' + title_data + '</h2><div class="excerpt">' + author_data + '<br> (' + publisher_data + ')</div></div></div></a>';
-          }
+          count++;
+	  }
 			});
 
 			$(".posts.simple").append(html_data);
